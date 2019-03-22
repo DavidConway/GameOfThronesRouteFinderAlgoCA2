247b1cc95e4ebf9c60d6aa81e136a0c8f369315b
Max Larkin <maxarastir@gmail.com>
2019-03-22 19:10:08 +0000

    Adding markers to map

--- GameOfThronesRouteFinderAlgoCA2/src/application/Controller.java
+++ GameOfThronesRouteFinderAlgoCA2/src/application/Controller.java
@@ -20,6 +20,18 @@ public class Controller {
 		mapAnchor.getChildren().add(mapPane);
 		mapPane.setFitWidth(Screen.getPrimary().getVisualBounds().getWidth());
 		mapAnchor.setMinWidth(mapPane.getFitWidth());
+		mapPane.setOnMouseClicked(e-> addNode(e.getX(), e.getY(), mapPane));
+	}
+
+	private void addNode(double x, double y, ImageView mapPane) {
+		System.out.println("Adding Node");
+		ImageView marker = new ImageView("/images/node.png");
+		marker.setFitWidth(15);
+		marker.setFitHeight(15);
+		marker.setX(x);
+		marker.setY(y);
+		mapAnchor.getChildren().add(marker);
+		
 	}
 
 }
